7d2373d647175675d6c634b6c148371f6ac9515e
adiyat-shihab <mohammadshihab104@gmail.com>
2023-09-26 07:06:23 +0600

    fix the bottom position bug in DonationDetails.jsx

--- src/Component/DonationDetalis/DonationDetails.jsx
+++ src/Component/DonationDetalis/DonationDetails.jsx
@@ -43,23 +43,27 @@ export const DonationDetails = () => {
           "mt-[5.19rem] px-4 xl:px-[8.75rem] md:px-8 lg:px-10 relative"
         }
       >
-        <img
-          src={picture}
-          alt=""
-          className={
-            " w-[25rem]  md:w-[44rem] lg:w-[69rem]  xl:w-[101.5rem] xl:h-[56.75rem]"
-          }
-        />
-        <div
-          className={
-            " px-4 py-4 bottom-[21.5rem] w-[24.5rem] md:w-[44rem] absolute md:bottom-[14.8rem] lg:bottom-[13.2rem] xl:bottom-[13.3rem] bg-[#0b0b0b80]  lg:w-[59rem]   xl:w-[72.5rem] 2xl:w-[101.5rem] md:px-[2.31rem] md:py-[2.31rem] md:h-[8.125rem] "
-          }
-        >
-          <button
-            className={"md:px-[1.62rem] px-2 py-2 md:py-4 text-white rounded "}
-            style={{ backgroundColor: text_button_bg_color }}
-            onClick={() => handleData(data)}
-          >{`Donate $${Math.floor(price)}`}</button>
+        <div className={"relative"}>
+          <img
+            src={picture}
+            alt=""
+            className={
+              " w-[25rem]  md:w-[44rem] lg:w-[69rem]  xl:w-[101.5rem] xl:h-[56.75rem]"
+            }
+          />
+          <div
+            className={
+              " px-4 py-4 bottom-0 w-[24.5rem] md:w-[44rem] absolute  bg-[#0b0b0b80]  lg:w-[59rem]   xl:w-[72.5rem] 2xl:w-[101.5rem] md:px-[2.31rem] md:py-[2.31rem] md:h-[8.125rem] "
+            }
+          >
+            <button
+              className={
+                "md:px-[1.62rem] px-2 py-2 md:py-4 text-white rounded "
+              }
+              style={{ backgroundColor: text_button_bg_color }}
+              onClick={() => handleData(data)}
+            >{`Donate $${Math.floor(price)}`}</button>
+          </div>
         </div>
         <h1 className={"mt-[3.5rem] mb-[1.5rem] text-[2.5rem] font-bold"}>
           {title}
